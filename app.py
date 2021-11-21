4db93f8b044ab54760b04be5f8829c5850d5b99b
Luke Bailey <baileyl1991@gmail.com>
2021-11-21 20:10:21 +0000

    add sess.init_app to fix runtime error

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+sess.init_app(app)
+
 
 mongo = PyMongo(app)
 
